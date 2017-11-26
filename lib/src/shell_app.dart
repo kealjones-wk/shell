1c49a1859c14595ea970c38d54824cda450daee0
Joel Leibow <joel.leibow@workiva.com>
2017-11-26 14:56:32 -0600

    Display shell messages

--- lib/src/shell_app.dart
+++ lib/src/shell_app.dart
@@ -6,11 +6,15 @@ UiFactory<ShellAppProps> ShellApp;
 @Props()
 class ShellAppProps extends UiProps {
   bool showMessages;
+
+  List messages;
 }
 
 @State()
 class ShellAppState extends UiState {
   bool showMessages;
+  
+  List messages;
 }
 
 @Component()
@@ -18,18 +22,20 @@ class ShellAppComponent extends UiStatefulComponent<ShellAppProps, ShellAppState
   @override
   Map getDefaultProps() => (newProps()
     ..showMessages = true
+    ..messages = []
   );
   
   @override
   Map getInitialState() => (newState()
     ..showMessages = props.showMessages
+    ..messages = props.messages
   );
   
   render() {
     return (Dom.div()..className = 'shell')(
       Dom.h2()('Using over_react 1.18.0'),
       _renderShellControls(),
-      _renderMessages()
+      _renderMessagesBox()
     );
   }
 
@@ -39,13 +45,24 @@ class ShellAppComponent extends UiStatefulComponent<ShellAppProps, ShellAppState
     );
   }
 
-  _renderMessages() {
+  _renderMessagesBox() {
     var classes = new ClassNameBuilder()
       ..add('shell__messages')
       ..add('shell__messages--hidden', !state.showMessages);
     
     return (Dom.div()..className = classes.toClassName())(
-      Dom.h4()('Messages')
+      Dom.h4()('Messages'),
+      _renderMessages()
     );
+  }
+
+  _renderMessages() {
+    List messages = [];
+
+    for (var i = 0; i < state.messages.length; i++) {
+      messages.add(Dom.em()(Dom.p()(state.messages[i])));
+    }
+
+    return messages;
   }
 }
