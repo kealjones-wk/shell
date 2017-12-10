1c3cc538ac33b0b11bb5b571f68437aeeeb6b0b9
Joel Leibow <joel.leibow@workiva.com>
2017-12-09 17:08:09 -0700

    I'm silly

--- web/index.dart
+++ web/index.dart
@@ -3,7 +3,7 @@ import 'dart:html';
 import 'package:react/react_dom.dart' as react;
 import 'package:react/react_client.dart';
 
-import 'package:shell/shell.dart';
+import 'package:shell/shell.dart' show ShellApp;
 
 void main() {
   setClientConfiguration();
